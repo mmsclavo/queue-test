0f1bfff2e55e92566c2223d8e73119543223120b
Mario Sclavo <mmsclavo@gmail.com>
2024-02-04 19:47:51 -0300

    start consumer string service

---
+++ consumer-service/src/test/java/br/com/mario/consumerservice/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mario.consumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
